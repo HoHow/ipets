62593d4f9a1bbad7f54b7aaefab077255ad41a54
wuhoho <wumingho@gmail.com>
2017-09-18 13:09:43 +0800

    更新app.js Access-Control-Allow-Origin

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add headers
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://13.114.3.214/')
+  res.setHeader('Access-Control-Allow-Origin', '*')
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
